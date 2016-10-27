ca556e99e5fd2fc1b06595a414b18c909254003d
sareduhh <sarahe.kaminski@gmail.com>
2016-10-27 14:41:22 -0400

    first commit

---
+++ Chatter/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Chatter.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Chatter.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+       
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private ApplicationUser CurrentUser
+        {
+            get
+            {
+                UserManager<ApplicationUser> UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+                ApplicationUser currentUser = UserManager.FindById(User.Identity.GetUserId());
+                return currentUser;
+            }
+        }
+        // GET: ApplicationUsers
+        public ActionResult Index()
+        {
+            var users = from u in db.Users
+                        orderby u.UserName
+                        select u;
+            return View(users.ToList());
+        }
+        public ActionResult Follow(string username)
+        {
+            var targetUser = db.Users.Single(u => u.UserName == username);
+            targetUser.Followers.Add(CurrentUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+
+
+
+    // GET: ApplicationUsers
+    //public ActionResult Index()
+    //{
+    //    return View(db.ApplicationUsers.ToList());
+    //}
+
+    //// GET: ApplicationUsers/Details/5
+    //public ActionResult Details(string id)
+    //{
+    //    if (id == null)
+    //    {
+    //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //    }
+    //    ApplicationUser applicationUser = db.ApplicationUsers.Find(id);
+    //    if (applicationUser == null)
+    //    {
+    //        return HttpNotFound();
+    //    }
+    //    return View(applicationUser);
+    //}
+
+    //// GET: ApplicationUsers/Create
+    //public ActionResult Create()
+    //{
+    //    return View();
+    //}
+
+    //// POST: ApplicationUsers/Create
+    //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+    //{
+    //    if (ModelState.IsValid)
+    //    {
+    //        db.ApplicationUsers.Add(applicationUser);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    return View(applicationUser);
+    //}
+
+    //// GET: ApplicationUsers/Edit/5
+    //public ActionResult Edit(string id)
+    //{
+    //    if (id == null)
+    //    {
+    //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //    }
+    //    ApplicationUser applicationUser = db.ApplicationUsers.Find(id);
+    //    if (applicationUser == null)
+    //    {
+    //        return HttpNotFound();
+    //    }
+    //    return View(applicationUser);
+    //}
+
+    //// POST: ApplicationUsers/Edit/5
+    //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+    //{
+    //    if (ModelState.IsValid)
+    //    {
+    //        db.Entry(applicationUser).State = EntityState.Modified;
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+    //    return View(applicationUser);
+    //}
+
+    //// GET: ApplicationUsers/Delete/5
+    //public ActionResult Delete(string id)
+    //{
+    //    if (id == null)
+    //    {
+    //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //    }
+    //    ApplicationUser applicationUser = db.ApplicationUsers.Find(id);
+    //    if (applicationUser == null)
+    //    {
+    //        return HttpNotFound();
+    //    }
+    //    return View(applicationUser);
+    //}
+
+    //// POST: ApplicationUsers/Delete/5
+    //[HttpPost, ActionName("Delete")]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult DeleteConfirmed(string id)
+    //{
+    //    ApplicationUser applicationUser = db.ApplicationUsers.Find(id);
+    //    db.ApplicationUsers.Remove(applicationUser);
+    //    db.SaveChanges();
+    //    return RedirectToAction("Index");
+    //}
+
+    //protected override void Dispose(bool disposing)
+    //{
+    //    if (disposing)
+    //    {
+    //        db.Dispose();
+    //    }
+    //    base.Dispose(disposing);
+    //}
+}
+
---
+++ Chatter/Models/IdentityModels.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+
+namespace Chatter.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<ApplicationUser> Followers { get; set; }
+        public virtual ICollection<ApplicationUser> Following { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public object ApplicationUsers { get; internal set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        //public System.Data.Entity.DbSet<Chatter.Models.ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
